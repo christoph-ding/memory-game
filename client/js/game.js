1b7dd90121af1e4ef1de8d7979df37a51ed64026
Pokegear <christoph.ding@gmail.com>
2016-05-25 22:55:47 -0400

    Planned on a high level, the stuff neccesary to make this game

---
+++ client/js/game.js
@@ -0,0 +1,43 @@
+// all the tiles
+var tiles = [];
+var images = [];
+
+// tile constructor
+  /* tile has:
+    column
+    row
+    image
+    id
+    how are two tiles 'the same'
+  */
+
+  /* 'render' facedown
+    maybe this is ... just css ..
+    draw the card's style: 
+  */
+
+  /* 'render' faceup
+
+  */
+
+// the game itself
+  /*
+    init?????
+      - render:
+        for each tile...
+          make the dom element
+          # USE A RECT
+
+      - set up game state:
+        shuffle the tiles
+        shuffle the images
+        for each tile...
+          give it an image          
+  */
+
+  /* functions we need to add to each tile
+    mouseClicking
+    
+  */
+
+
